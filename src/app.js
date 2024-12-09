b9dc77c6bd0deb8cd8af6b59ac214d42b2fd214b
David Rosas <156865176+davidrosas809@users.noreply.github.com>
2024-12-09 23:28:00 +0000

    finishing my Escusesgenerator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,35 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let who = ["Mi Gato", "Michael Jackson", "El Rubius", "Notorious BIG"];
+let action = ["se robo", "tiro a la basura", "se fumo", "no me devolvio"];
+let what = ["mi pantalon", "mi dinero", "la tarea"];
+let when = [
+  "despues de la fiesta.",
+  "cuando fuimos a acampar.",
+  "mientras robabamos un banco.",
+  "en la cena de negocios.",
+  "mientras lo carreaba en lol.",
+];
+
+function Elegiraleatorio(lista) {
+  let randomnumber = Math.random() * lista.length;
+  randomnumber = Math.floor(randomnumber);
+  return lista[randomnumber];
+}
+
+let excuse =
+  Elegiraleatorio(who) +
+  " " +
+  Elegiraleatorio(action) +
+  " " +
+  Elegiraleatorio(what) +
+  " " +
+  Elegiraleatorio(when);
+
+let excusegenerator = document.querySelector("#Excusesgenerator");
+excusegenerator.textContent = excuse;
+console.log(excusegenerator);
